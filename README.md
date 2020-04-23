b609fa6e973c6468a9575d8c266cbe6db1c8fc70
josephgatonye <student27@dee2-dataprocess1.novalocal>
2020-04-23 12:04:11 +1000

    created new Rscript to download and read in tabular data file in R. Update to the README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # example-3-repeat
 In  class demonstration about how to use Github via Rstudio
+
+##Motivation 
+Help students learn Github and markdown format
+
+##Contents 
+* Readme file in markdown
+* example Rscript/Rmd
+
+##Installation
+RStudio Version 1.2.1335 and R version 3.6.3
+
+##Credits
+Joseph Gatonye
---
+++ example.R
@@ -0,0 +1,10 @@
+# Download the data
+download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part1_files/mydata.tsv",destfile = "mydata.tsv")
+
+#Read table into R making sure the 1st column is set as row names and headers recognized
+x <- read.table("mydata.tsv", header = TRUE, row.names = 1,stringsAsFactors = FALSE)
+
+# check if the data  has been imported properly
+head(x)
+str(x)
+
